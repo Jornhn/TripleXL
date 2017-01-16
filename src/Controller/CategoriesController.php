fe134f3cb77f0cd7b49e4afc5494107fe3dc341a
Jos Prins <j.a.prins@hotmail.com>
2017-01-16 13:30:15 +0100

    Categories
    - Deleted __consturct()
    - Word niet gebruikt

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -6,9 +6,6 @@ use Cake\Network\Response;
 
 class CategoriesController extends AppController{
 
-    public function __construct($request = null, $response = null, $name = null, $eventManager = null, $components = null){
-        parent::__construct($request, $response, $name, $eventManager, $components);
-    }
 
     public function isAuthorized($user){
         if (isset($user['account_type']) && $user['account_type'] >= '2') {
