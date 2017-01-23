31d26755858d804814fb40fb7b682a09af2c6da9
Jos Prins <j.a.prins@hotmail.com>
2017-01-23 20:50:10 +0100

    I did it wrong again LOL

---
+++ src/Model/Table/PurchasesTable.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sven
+ * Date: 12-1-2017
+ * Time: 11:27
+ */
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class PurchasesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        
+    }
+}
